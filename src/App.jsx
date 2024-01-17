b9be0db446143a4aa43e59b3795d4d4cd5831dc3
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-17 23:08:31 +0600

    Header file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1>this is my welcome page</h1>
+      <Header></Header>
     </>
   )
 }
