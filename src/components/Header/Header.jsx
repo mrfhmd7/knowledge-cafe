c35e1203d82e372ea279979a7fe9eb819561feec
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-18 21:00:59 +0600

    nav bar added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Header = () => {
      return (
-          <div>
-               <h1>This is header section.</h1>
-          </div>
+          <nav className='flex justify-between h-20 rounded-md items-center bg-indigo-600'>
+               <div className='ml-5'>
+                    <h3 className='text-white font-semibold '>Knowledge Cafe</h3>
+               </div>
+               <div className='mr-5'>
+                    <img className='h-12 w-12 object-cover rounded-full' src="khala-login-picture.jpg" alt="" />
+               </div>
+          </nav>
      );
 };
 
