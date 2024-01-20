227c00ddcada7011d1177b34d6fd91e03fa812d5
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-20 16:09:57 +0600

    bookmarked added

---
+++ src/components/Bookmarked/Bookmarked.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bookmarked = () => {
+     return (
+          <div>
+               <h1>This is bookmarked.</h1>
+          </div>
+     );
+};
+
+export default Bookmarked;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Bookmarked from '../Bookmarked/Bookmarked';
 
 const Sidebar = () => {
      return (
@@ -7,7 +8,7 @@ const Sidebar = () => {
                     <h3 className='text-center mt-4 text-blue-700 font-semibold text-xl'>Spent time on read :</h3>
                </div>
                <div>
-                    <h3>Bookmarked Blog:</h3>
+                    <Bookmarked></Bookmarked>
                </div>
           </>
      );
