9846ce00b10978779bf3edd5587c4f695c55a3e8
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-20 18:02:05 +0600

    syle finished in sidebar

--- src/components/Bookmarked/Bookmarked.jsx
+++ src/components/Bookmarked/Bookmarked.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Bookmarked = () => {
      return (
-          <div>
-               <h1>This is bookmarked.</h1>
+          <div className='bg-white h-24 rounded-md text-center pt-3 m-auto'>
+               <h1 className='font-bold text-2xl'>This is bookmarked.</h1>
           </div>
      );
 };
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,10 +5,13 @@ const Sidebar = () => {
      return (
           <>
                <div className='item-center border-solid border-2 border-blue-400 bg-pink-100 rounded-lg h-16'>
-                    <h3 className='text-center mt-4 text-blue-700 font-semibold text-xl'>Spent time on read :</h3>
+                    <h3 className='text-center mt-4 text-blue-700 font-semibold text-2xl'>Spent time on read:</h3>
                </div>
-               <div>
-                    <Bookmarked></Bookmarked>
+               <div className='mt-8 pl-7 bg-zinc-200 rounded-lg h-auto'>
+                    <h3 className='font-bold text-2xl pt-3'>Bookmarked Blogs: </h3>
+                    <div className=' pr-7 mt-5 pb-5'>
+                         <Bookmarked></Bookmarked>
+                    </div>
                </div>
           </>
      );
